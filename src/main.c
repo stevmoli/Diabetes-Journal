44ba7208a58526d7787f995bac892fbea5c1f139
stevmoli <stevmoli@users.noreply.github.com>
2015-04-04 08:29:34 -0400

    Scroll layer implemented

--- src/main.c
+++ src/main.c
@@ -3,16 +3,53 @@
 #include <stdlib.h>
 
 Window *my_window;
+//Window *history_window;
 TextLayer *sugar_layer;
 TextLayer *saved_layer;
+ScrollLayer *history_scroll;
+
+TextLayer *history_text;
+static int WIDTH = 144;
+static int MAX_HEIGHT = 1000;
+static int TOP_BAR_PADDING = 20;
+
 int sugar = 85;
 static char sugar_buffer[] = "000";
 //char dates[50]; 
 int sugar_history[50];
-int sugar_history_index = 0;
+int size = 0;
+int center_press = true;
+
+void sugar_data_retrieve (int size) {
+  for (int i = 0; i < size; i++) {
+    snprintf(sugar_buffer, sizeof(sugar_buffer), "%d", sugar_history[i]);
+    text_layer_set_text(history_text, sugar_buffer);
+  }
+//  return history_text;
+}
+
+void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+  //scroll_layer_create(history_scroll, GRect(0, 0, 144, 168));
+  history_scroll = scroll_layer_create GRect(0,0,144,168);
+  scroll_layer_set_click_config_onto_window(history_scroll, my_window);
+  history_text = text_layer_create(GRect(0, 0, WIDTH, MAX_HEIGHT));
+  sugar_data_retrieve(size);
+  text_layer_set_background_color(history_text, GColorClear);
+  text_layer_set_text_color(history_text, GColorBlack);
+  text_layer_set_text_alignment(history_text, GTextAlignmentLeft);
+  scroll_layer_set_click_config_onto_window(history_scroll, my_window);
+  
+  
+  scroll_layer_add_child(history_scroll, text_layer_get_layer(history_text));
+  layer_add_child(window_get_root_layer(my_window), scroll_layer_get_layer(history_scroll));
+}
+
+void select_long_click_release_handler() {
+  
+}
 
 void save_sugar() {
-  sugar_history[sugar_history_index++] = sugar;
+  sugar_history[size++] = sugar;
   text_layer_set_text(saved_layer, "Blood sugar saved!");
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(saved_layer));
 }
@@ -37,7 +74,28 @@ void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_long_click_subscribe(BUTTON_ID_SELECT,  700, select_long_click_handler, select_long_click_release_handler);
 }
+
+/*
+void history_up_click_handler () {
+  
+}
+
+void history_down_click_handler () {
+  
+}
+
+void history_select_click_handler () {
+  
+}
+
+void history_click_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_UP, history_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, history_down_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, history_select_click_handler);
+}
+*/
 
 void window_load (Window *my_window) {
   sugar_layer = text_layer_create(GRect(0, 50, 144, 120));
@@ -48,12 +106,11 @@ void window_load (Window *my_window) {
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(sugar_layer));
   snprintf(sugar_buffer, sizeof(sugar_buffer), "%d", sugar);
   
-  saved_layer = text_layer_create(GRect(0,0,144,166));
+  saved_layer = text_layer_create(GRect(0,0,144,168));
   text_layer_set_background_color(saved_layer, GColorWhite);
   text_layer_set_text_color(saved_layer, GColorBlack);
   text_layer_set_text_alignment(saved_layer, GTextAlignmentCenter);
   text_layer_set_font(saved_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  
   text_layer_set_text(sugar_layer, sugar_buffer);
 }
 
@@ -70,6 +127,15 @@ void handle_init(void) {
   window_set_click_config_provider(my_window, click_config_provider);
   sugar_layer = text_layer_create(GRect(0, 0, 144, 20));
   window_stack_push(my_window, true);
+  
+  /*
+  history_window = window_create();
+  window_set_window_handlers(history_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_set_click_config_provider(history_window, history_click_provider); 
+  */
 }
 
 void handle_deinit(void) {
